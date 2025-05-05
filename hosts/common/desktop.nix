8c9deffda76782cb09cc5892fd7e6d3dcf884b7a
ponfertato <ponfertato@ya.ru>
2025-05-05 22:44:45 +0300

    🎉init blocky

--- hosts/common/desktop.nix
+++ hosts/common/desktop.nix
@@ -4,6 +4,57 @@
       enable = true;
       xkb.layout = "us,ru";
     };
+    blocky = {
+      enable = true;
+      settings = {
+        ports = {
+          dns = "53";
+          tls = "853";
+          https = "443";
+        };
+        minTlsServeVersion = "1.3";
+        connectIPVersion = "dual";
+        upstreams = {
+          groups = {
+            default = [
+              "https://one.one.one.one/dns-query"
+              "https://dns.google/dns-query"
+            ];
+          };
+          strategy = "parallel_best";
+          timeout = "5s";
+        };
+        bootstrapDns = {
+          upstream = "https://one.one.one.one/dns-query";
+          ips = ["1.1.1.1" "1.0.0.1"];
+        };
+        blocking = {
+          denylists = {
+            ads = [
+              "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
+              "https://schakal.ru/hosts/hosts.txt"
+            ];
+          };
+          clientGroupsBlock = {
+            default = [ "ads" ];
+          };
+          blockType = "nxDomain";
+          blockTTL = "1h";
+        };
+        caching = {
+          minTime = "5m";
+          maxTime = "1h";
+          prefetching = true;
+          cacheTimeNegative = "15m";
+        };
+        ede.enable = true;
+        ecs = {
+          ipv4Mask = 24;
+          ipv6Mask = 56;
+        };
+        fqdnOnly.enable = true;
+      };
+    };
     displayManager.sddm.enable = true;
     desktopManager.plasma6.enable = true;
     printing.enable = true;
