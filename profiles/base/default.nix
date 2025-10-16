6307bf6df61e7555ccdb4582689964bf029322c8
ponfertato <ponfertato@ya.ru>
2025-10-16 09:32:08 +0300

    🚧fix substituters
    https://nix.dev/manual/nix/2.28/command-ref/conf-file.html#conf-trusted-public-keys

--- profiles/base/default.nix
+++ profiles/base/default.nix
@@ -41,12 +41,13 @@
         "flakes"
       ];
       substituters = [
-        "https://cache.nixos.org/"
-        "https://cache.garnix.io"
+        "https://cache.nixos.org"
         "https://nixos.snix.store"
+        "https://cache.garnix.io"
         "https://nixos-cache-proxy.cofob.dev"
       ];
       trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       ];
     };
