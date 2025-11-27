7c19f6149b1e3741ccf1bc03ad733e4c43c1b8ed
ponfertato <ponfertato@ya.ru>
2025-11-27 13:00:52 +0300

    🚑️fix hardware.bluetooth

--- profiles/laptop/default.nix
+++ profiles/laptop/default.nix
@@ -1,6 +1,15 @@
 { config, pkgs, ... }:
 {
-  hardware.bluetooth.enable = true;
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        ControllerMode = "bredr";
+      };
+    };
+    disabledPlugins = [ "sap" ];
+  };
 
   services = {
     fprintd.enable = true;
