94b49689881f95c1250ba6dc0c78b3621a809b00
ponfertato <ponfertato@ya.ru>
2025-12-24 11:45:15 +0300

    🚧fix substituters
    remove tvix.store and doubling public-keys

--- profiles/base/default.nix
+++ profiles/base/default.nix
@@ -59,12 +59,10 @@
         "https://nix-community.cachix.org"
         "https://nix-gaming.cachix.org"
         "https://nixos.snix.store"
-        "https://nixos.tvix.store"
       ];
       
       trusted-public-keys = [
         "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
