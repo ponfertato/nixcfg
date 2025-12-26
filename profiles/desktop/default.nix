a1d862ae60224fd1932a63d25e9c7e6efa16f115
ponfertato <ponfertato@ya.ru>
2025-12-26 17:19:08 +0300

    ✨add environment

--- profiles/desktop/default.nix
+++ profiles/desktop/default.nix
@@ -1,5 +1,20 @@
 { config, pkgs, ... }:
 {
+  environment.sessionVariables = {
+    GDK_BACKEND = "wayland,x11";
+    LANG = "ru_RU.UTF-8";
+    LC_TIME = "ru_RU.UTF-8";
+    MOZ_ENABLE_WAYLAND = "1";
+    NIXOS_OZONE_WL = "1";
+    PULSE_PROP = "media.role:music";
+    QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+    QT_QPA_PLATFORM = "wayland";
+    QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+    SDL_VIDEODRIVER = "wayland";
+    XDG_CURRENT_DESKTOP = "KDE";
+    XDG_SESSION_TYPE = "wayland";
+  };
+
   services = {
     desktopManager.plasma6.enable = true;
     xserver = {
